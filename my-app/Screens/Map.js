38fc1e548e5e7f63046ccdb1868e7c57cc9a73f1
nguyenq9 <nguyenq9@wwu.edu>
2023-11-17 12:40:31 -0800

    added buttons to map

--- my-app/Screens/Map.js
+++ my-app/Screens/Map.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Button, Text, View, StyleSheet } from "react-native";
+import { Button, Text, View, StyleSheet, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import MapView from "react-native-maps";
 
 const styles = StyleSheet.create({
@@ -11,6 +12,34 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
+  buttonCallout: {
+    position: 'absolute',
+    bottom: 50,
+    left: 20, // Set the left position to your desired value
+    backgroundColor: "transparent",
+    borderWidth: 0.5,
+    borderRadius: 20,
+    backgroundColor: "white",
+  },
+  buttonCallout2: {
+    position: 'absolute',
+    bottom: 120,
+    left: 20, // Set the left position to your desired value
+    backgroundColor: "transparent",
+    borderWidth: 0.5,
+    borderRadius: 20,
+    backgroundColor: "white",
+  },
+  buttonCallout3: {
+    position: 'absolute',
+    bottom: 190,
+    left: 20, // Set the left position to your desired value
+    backgroundColor: "transparent",
+    borderWidth: 0.5,
+    borderRadius: 20,
+    backgroundColor: "white",
+  },
+  
 });
 
 function Map() {
@@ -26,7 +55,20 @@ function Map() {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        showUserLocation={true}
+        showsCompass={true}
       />
+      <Pressable style={styles.buttonCallout} onPress={() => console.log("magnify MINUS")}>
+                <MaterialCommunityIcons name="magnify-minus-outline" color={"black"} size={50} />
+      </Pressable>
+
+      <Pressable style={styles.buttonCallout2} onPress={() => console.log("magnify PLUS")}>
+                <MaterialCommunityIcons name="magnify-plus-outline" color={"black"} size={50} />
+      </Pressable>
+
+      <Pressable style={styles.buttonCallout3} onPress={() => console.log("CREATING WAYPOINT")}>
+                <MaterialCommunityIcons name="map-marker" color={"black"} size={50} />
+      </Pressable>
     </View>
   );
 }
